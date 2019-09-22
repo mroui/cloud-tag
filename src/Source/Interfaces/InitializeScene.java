4127c60a060f3b8586326df0bf6e706f5df3faf5
Martyna Roj <martynaxroj@gmail.com>
2019-09-22 19:14:55 +0200

    Improve readability of code in InitializeScene interface

--- src/Source/Interfaces/InitializeScene.java
+++ src/Source/Interfaces/InitializeScene.java
@@ -4,9 +4,9 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+
 
 public interface InitializeScene {
 
