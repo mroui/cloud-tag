c7f3460cb56ed599a21f87250c6af9f50088530e
Martyna Roj <martynaxroj@gmail.com>
2019-09-22 18:18:53 +0200

    Add initializing scene interface with init() default method

---
+++ src/Source/InitializeScene.java
@@ -0,0 +1,22 @@
+package Source;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public interface InitializeScene {
+
+    default void init(Button exitButton, Pane pane, String xImagePath) {
+        try {
+            Image xImage = new Image(new FileInputStream(xImagePath));
+            exitButton.setGraphic(new ImageView(xImage));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        pane.setOpacity(0);
+    }
+}
